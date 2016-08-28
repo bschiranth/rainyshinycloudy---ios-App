8849722a277cb5e8c7132c79a8c74299e46468b1
bschiranth <bschiranth@gmail.com>
2016-08-28 05:36:54 -0400

    working version
    shows weather date

---
+++ rainyshinycloudy/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  rainyshinycloudy
+//
+//  Created by Caleb Stultz on 7/27/16.
+//  Copyright © 2016 Caleb Stultz. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "d2965038cabd4ec6d017e47795f855e5"
+
+typealias DownloadComplete = () -> ()
+
+let CURRENT_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&appid=d2965038cabd4ec6d017e47795f855e5"
+let FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&cnt=10&mode=json&appid=d2965038cabd4ec6d017e47795f855e5"
